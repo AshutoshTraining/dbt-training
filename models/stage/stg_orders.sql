234c1c92decf337ba0186b525ecb1ba7b31fca35
pandeyashu89@gmail.com <pandeyashu89@gmail.com>
2023-02-17 15:41:08 +0000

    increamental table on raw_orders

--- models/stage/stg_orders.sql
+++ models/stage/stg_orders.sql
@@ -1,6 +1,7 @@
 select
-    {{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as surr_key,
-    -- from raw_orders
+    {{ dbt_utils.generate_surrogate_key(["o.orderid", "c.customerid", "p.productid"]) }}
+    as surr_key,
+    {# from raw_orders -- -- {{limit_data_in_dev('orderdate')}} #}
     o.orderid,
     o.orderdate,
     o.shipdate,
@@ -16,10 +17,9 @@ select
     p.productname,
     p.category,
     p.subcategory,
-    {{ markup('ordersellingprice','ordercostprice') }} as profitperct
-  --  d.delivery_team
+    {{ markup("ordersellingprice", "ordercostprice") }} as profitperct,
+    d.delivery_team
 from {{ ref("raw_orders") }} as o
 left join {{ ref("raw_customers") }} as c on o.customerid = c.customerid
 left join {{ ref("raw_products") }} as p on o.productid = p.productid
-left join {{ref("delivery_team")}} as d on o.shipmode= d.shipmode
---{{limit_data_in_dev('orderdate')}}
+left join {{ ref("delivery_team") }} as d on o.shipmode = d.shipmode
